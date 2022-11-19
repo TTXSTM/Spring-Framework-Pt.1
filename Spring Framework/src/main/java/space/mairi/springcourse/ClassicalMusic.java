60dcf901d5dde61c90e7593f51e83016b336bade
TTXSTM <71548215+TTXSTM@users.noreply.github.com>
2022-11-19 20:19:15 +0300

    Inversion of Control (IoC)
    Used IoC, and corrected 3 major errors

---
+++ Spring Framework/src/main/java/space/mairi/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package space.mairi.springcourse;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ Spring Framework/src/main/java/space/mairi/springcourse/RockMusic.java
@@ -0,0 +1,8 @@
+package space.mairi.springcourse;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Smells Like Teen Spirit";
+    }
+}
